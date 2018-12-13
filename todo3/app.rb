cd69661f2e2485ba404bba68130a42aca7fd8e3b
halllllll <h4l@yahoo.ne.jp>
2018-12-13 14:38:05 +0900

    rubocopいれてauto-correctしたお

--- todo3/app.rb
+++ todo3/app.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'digest/md5'
 require 'time'
 require 'date'
 
-set :todos => {}
-FILENAME = "todo.json"
+set todos: {}
+FILENAME = 'todo.json'
 
 class Todo
   attr_accessor :title, :content, :date, :priority, :id
@@ -13,20 +15,21 @@ class Todo
     @date = Time.new.iso8601(6).to_s
     @id = Digest::MD5.hexdigest(@date)
   end
+
   def profile_to_hash
-    self.instance_variables.map{|var|
+    instance_variables.map do |var|
       # そのままだと頭に@つきになってしまうので
       # [var.match(/[\w\d]+/).to_s, self.instance_variable_get(var)]
       # と思ったけど@ごと文字列にします
-      [var.to_s, self.instance_variable_get(var)]
-    }.to_h
+      [var.to_s, instance_variable_get(var)]
+    end.to_h
   end
 end
 
 # エントリポイント
 get '/' do
-  if File.exists?(FILENAME)
-    File.open(FILENAME, 'r+') do  |file|
+  if File.exist?(FILENAME)
+    File.open(FILENAME, 'r+') do |file|
       # 読み込み
       settings.todos = JSON.load(file)
     end
@@ -44,11 +47,11 @@ get '/todos/new' do
 end
 
 post '/' do
-  if params[:todocontent].split.size==0 && params[:todotitle].split.size==0
+  if params[:todocontent].split.empty? && params[:todotitle].split.empty?
     status 204
     redirect '/'
   end
-  todo = Todo.new()
+  todo = Todo.new
   todo.content = params[:todocontent].strip
   todo.title = params[:todotitle].strip
   settings.todos[todo.id] = todo.profile_to_hash
@@ -62,10 +65,10 @@ end
 
 # 編集済データ更新(patchメソッド)
 patch '/todos/:id' do
-  settings.todos[params["id"]]["@title"] = params[:todotitle].strip
-  settings.todos[params["id"]]["@content"] = params[:todocontent].strip
+  settings.todos[params['id']]['@title'] = params[:todotitle].strip
+  settings.todos[params['id']]['@content'] = params[:todocontent].strip
   # 時間を編集した時点に更新
-  settings.todos[params["id"]]["@date"] = Time.new.iso8601(6).to_s
+  settings.todos[params['id']]['@date'] = Time.new.iso8601(6).to_s
   updatelist(settings.todos, FILENAME)
   @todos = settings.todos
   # もうちょい検証が必要..?
@@ -75,14 +78,14 @@ end
 
 get '/todos/:id' do |i|
   @todo_id = i
-  @todo_title = settings.todos[i]["@title"].strip
-  @todo_content = settings.todos[i]["@content"].strip
+  @todo_title = settings.todos[i]['@title'].strip
+  @todo_content = settings.todos[i]['@content'].strip
   erb :todo
 end
 
 # deleteメソッド
 delete '/todos/:id' do
-  settings.todos.delete(params["id"])
+  settings.todos.delete(params['id'])
   updatelist(settings.todos, FILENAME)
   @todos = settings.todos
   status 204
@@ -91,8 +94,8 @@ end
 
 get '/todos/:id/edit' do |i|
   @todo_id = i
-  @todo_title = settings.todos[i]["@title"].strip
-  @todo_content = settings.todos[i]["@content"].strip
+  @todo_title = settings.todos[i]['@title'].strip
+  @todo_content = settings.todos[i]['@content'].strip
   erb :edit
 end
 
@@ -100,8 +103,8 @@ helpers do
   # jsonを現在のtodoに合わせてアップデート
   def updatelist(dict, file)
     # 時間順に並び替える
-    dict = dict.sort_by do |key, val|
-      val["@date"]
+    dict = dict.sort_by do |_key, val|
+      val['@date']
     end.reverse.to_h
     open(file, 'w+') do |f|
       JSON.dump(dict, f)
@@ -111,8 +114,8 @@ helpers do
   end
 
   def gen_list(k, todo)
-    tex = todo["@title"].size.zero? ? todo["@content"][0, 20] : todo["@title"]
-    lastupdate = " -- [" + DateTime.parse(todo["@date"]).strftime("%b %d %H:%M") + "]"
+    tex = todo['@title'].size.zero? ? todo['@content'][0, 20] : todo['@title']
+    lastupdate = ' -- [' + DateTime.parse(todo['@date']).strftime('%b %d %H:%M') + ']'
     " <li class='todo__item'><a href='/todos/#{k}'>#{tex}</a><i class=&todo__item__updatetime'>#{lastupdate}</i></li>"
   end
 end
